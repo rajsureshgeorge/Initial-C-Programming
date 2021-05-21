64a9224d05139d30f0176982fb53848ab14bb93f
Raj <rajsuresh1999@gmail.com>
2021-05-21 22:37:10 +0530

    Deleted and Added Again

---
+++ True/src/True.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a,b;
+	printf("Enter 2 Number");
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if (a>b)
+	{
+		printf("Number is greater");
+	}
+	else
+	{
+		printf("Number Is Lesser");
+	}
+
+
+}
