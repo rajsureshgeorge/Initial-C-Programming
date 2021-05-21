66a6c06e6fc75a2696b0f92eee481a4d5e8ea424
Raj <rajsuresh1999@gmail.com>
2021-05-22 05:05:39 +0530

    Basic Maths

--- True/src/True.c
+++ True/src/True.c
@@ -2,17 +2,31 @@
 #include <stdlib.h>
 
 int main(void) {
-	int a,b;
+	int a,b,c;
+	printf("The Application For Basic Maths\n");
+	printf("Enter Your Choice\n");
+	printf("1. Addition \n2.Substraction \n3.Multiplication \n4.division \nEnter What You Need:");
+	scanf("%d",&c);
 	printf("Enter 2 Number");
-	scanf("%d",&a);
-	scanf("%d",&b);
-	if (a>b)
+	scanf("%d%d",&a,&b);
+	if (c==1)
 	{
-		printf("Number is greater");
+		c=a+b;
+		printf("Sum is %d",c);
+	}
+	else if(c==2){
+		c=a-b;
+		printf("Sub is %d",c);
+	}
+
+	else if(c==3){
+		c=a*b;
+		printf("mul is %d",c);
 	}
 	else
 	{
-		printf("Number Is Lesser");
+		c=a/b;
+		printf("div is %d",c);
 	}
 
 
