bff67ab3e0ca3215481913c0975acb8b44daaf4d
Raj <rajsuresh1999@gmail.com>
2021-05-22 19:45:29 +0530

    Sorting

---
+++ sorting/src/sorting.c
@@ -0,0 +1,41 @@
+/*
+ ============================================================================
+ Name        : sorting.c
+ Author      : Raj
+ Version     :
+ Copyright   : 
+ Description : Sort
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int a[100];
+			int i,limit,j,temp;
+			printf("Enter the Array Limit: ");
+			scanf("%d",&limit);
+			printf("Enter the Array Numbers");
+			for (i=0;i<limit;i++)
+			{
+			printf(": ");
+				scanf("%d",&a[i]);
+			}
+			for(i=0;i<limit-1;i++)
+			{
+				for (j=i+1;j<limit;j++)
+				{
+					if(a[i]>a[j]){
+						temp=a[i];
+						a[i]=a[j];
+						a[j]=temp;
+					}
+				}
+			}
+			printf("Sorted\n");
+			for (i=0;i<limit;i++)
+						{
+						printf("%d\t",a[i]);
+						}
+}
