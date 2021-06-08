9be8eae5f13ed532307fb0ae26d86d4faa96d592
Raj <rajsuresh1999@gmail.com>
2021-06-08 19:26:32 +0530

    Input From USer Program

---
+++ input from user/src/input from user.c
@@ -0,0 +1,20 @@
+/*
+ ============================================================================
+ Name        : Name.c
+ Author      : Raj
+ Version     :
+ Copyright   : 
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i;
+				printf("Enter your name: ");
+				scanf("%d",&i);
+			printf("Your name is %d",i);
+
+}
